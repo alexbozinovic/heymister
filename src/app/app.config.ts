7042c0a775b5487ef6f6e4ebbaa6d39719f32a42
Bozinovic <abozinovic@dd61qn04v6.mi.corp.rockfin.com>
2026-05-07 18:30:09 -0400

    configured firebase and created logos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
